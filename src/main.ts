f304e91f4b1b338bdf8d3efd1e0ce62ab36fddf3
Denys Malikov <ryki.admirala@gmail.com>
2024-07-11 11:13:30 +0300

    Add fastify

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 const port = 3000;
 const logger = new Logger('main.ts');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  await app.listen(port, '0.0.0.0');
 
   logger.log(`Server running on port ${port}`);
 }
