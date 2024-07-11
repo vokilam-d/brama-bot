4dad1dade8bfdd5c1e844c5290da219790d55c53
Denys Malikov <ryki.admirala@gmail.com>
2024-07-11 11:17:36 +0300

    Add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(config.mongoUri, { retryDelay: 1 }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+import * as process from 'node:process';
+
+export const config = {
+  mongoUri: process.env.MONGO_URI,
+};
